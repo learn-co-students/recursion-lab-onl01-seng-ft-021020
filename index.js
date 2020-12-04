186125fe5220a04db6c1c3167adaf3837175ba2c
github username <schacter.jon@gmail.com>
2020-12-04 14:44:12 -0800

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,53 @@
 // Code your solution here!
 
+function printString(myString) {
+    console.log(myString[0]);
+
+    if (myString.length > 1) {
+        let mySubString = myString.substring(1, myString.length);
+        printString(mySubString);
+    } else {
+        return true;
+    }
+}
+
+function reverseString(myString) {
+    if (myString === "") {
+        return ""
+    } else {
+        return reverseString(myString.substring(1, myString.length)) + myString[0]
+    }
+}
+
+function isPalindrome(myString) {
+    let len = myString.length;
+
+    if (len <= 1) return true;
+    if (myString[0] !== myString[len - 1]) return false;
+
+    return isPalindrome(myString.slice(1, -1));
+}
+
+function addUpTo(myArray, i) {
+    if (i === 0) return myArray[0];
+
+    return addUpTo(myArray.slice(0, -1), i-1) + myArray[i];
+}
+
+function maxOf(myArray) {
+    if (myArray.length == 1) return myArray[0];
+
+    let n = maxOf(myArray.slice(1))
+    if (myArray[0] > n) {
+        return myArray[0];
+    } else {
+        return n;
+    }
+}
+
+function includesNumber(myArray, n) {
+    if (myArray.length === 0) return false;
+    if (myArray[0] === n) return true;
+
+    return includesNumber(myArray.slice(1), n)
+}
